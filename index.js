0ef7f8a1a85d438145f8a0d0d9024f3cf56e878a
Jaison Labrador <labradorjaison@gmail.com>
2024-12-16 08:39:07 -0500

    Updated CloudFormation, Lambda handler, and GitHub Actions pipeline

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 exports.handler = async (event) => {
   console.log("Event received:", JSON.stringify(event, null, 2));
 
-  // Aquí puedes implementar la lógica específica de tu proyecto
+  // Aquí puedes implementar la lógica específica de tu proyecto.
+  // Por ejemplo, puedes procesar el evento y publicar en SNS o almacenar en una base de datos.
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Lambda executed successfully!" }),
