ca32d45f43d01fb856cbefdab23df8857702c82d
Jaison Labrador <labradorjaison@gmail.com>
2024-12-16 18:34:03 -0500

    Updated Lambda to filter only the original message for email notifications

--- index.js
+++ index.js
@@ -5,41 +5,29 @@ const MAX_MESSAGE_SIZE = 256 * 1024; // 256 KB, el límite de SNS
 
 exports.handler = async (event) => {
   try {
-    console.log("Event received:", JSON.stringify(event, null, 2));
+    const message = JSON.parse(event.Records[0].body);
 
-    for (const record of event.Records) {
-      const message = JSON.parse(record.body);
-      console.log("Processing message:", message);
-
-      // Filtrar solo el mensaje original
-      const originalMessage = message.Message;
-
-      // Verificar el tamaño del mensaje
-      const messageString = JSON.stringify(originalMessage);
-      if (Buffer.byteLength(messageString, 'utf8') > MAX_MESSAGE_SIZE) {
-        console.log('Message is too large, truncating...');
-        originalMessage = originalMessage.substring(0, MAX_MESSAGE_SIZE); // O cualquier lógica para reducir el mensaje
-      }
-
-      // Publicar solo el mensaje original
-      const snsParams = {
-        Message: originalMessage,
-        TopicArn: 'arn:aws:sns:us-east-1:590183865524:Alertify-Inc-CriticalEvents-Unique',
-      };
-
-      await sns.publish(snsParams).promise();
-      console.log('Message sent to SNS:', snsParams.Message);
+    // Filtro: Verificar si el mensaje ya fue procesado
+    if (message.alreadyProcessed) {
+      console.log('Mensaje ya procesado, no enviando a SNS');
+      return;
     }
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Processing completed successfully!' }),
+    // Marcar el mensaje como procesado
+    message.alreadyProcessed = true;
+
+    const snsParams = {
+      Message: JSON.stringify(message),
+      TopicArn: 'arn:aws:sns:us-east-1:590183865524:Alertify-Inc-CriticalEvents-Unique',
     };
+
+    await sns.publish(snsParams).promise();
+    console.log('Mensaje enviado a SNS');
+
+    return { statusCode: 200, body: JSON.stringify({ message: 'Exitoso' }) };
+
   } catch (error) {
-    console.error('Error processing message:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error processing event' }),
-    };
+    console.error('Error:', error);
+    return { statusCode: 500, body: JSON.stringify({ message: 'Error' }) };
   }
 };
