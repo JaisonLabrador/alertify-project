4791c8d55f53328a47d4b9b14dd48f5a5c3471f1
Jaison Labrador <labradorjaison@gmail.com>
2024-12-16 10:04:47 -0500

    Update Lambda code to integrate with SNS and SQS

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+const sqs = new AWS.SQS();
+
 exports.handler = async (event) => {
-  console.log("Event received:", JSON.stringify(event, null, 2));
+  try {
+    console.log("Event received:", JSON.stringify(event, null, 2));
 
-  // Aquí puedes implementar la lógica específica de tu proyecto.
-  // Por ejemplo, puedes procesar el evento y publicar en SNS o almacenar en una base de datos.
+    // Iterar sobre los mensajes de SQS
+    for (const record of event.Records) {
+      const message = JSON.parse(record.body);
+      console.log("Processing message:", message);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Lambda executed successfully!" }),
-  };
+      // Publicar el mensaje en el SNS Topic
+      const snsParams = {
+        Message: JSON.stringify(message),
+        TopicArn: 'arn:aws:sns:us-east-1:590183865524:Alertify-Inc-CriticalEvents-Unique', // ARN del SNS Topic
+      };
+
+      await sns.publish(snsParams).promise();
+      console.log('Message sent to SNS:', snsParams.Message);
+
+      // Eliminar el mensaje de la cola SQS después de procesarlo
+      const deleteParams = {
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/590183865524/Alertify-Inc-EventQueue-Unique',  // URL de la SQS Queue
+        ReceiptHandle: record.receiptHandle,
+      };
+
+      await sqs.deleteMessage(deleteParams).promise();
+      console.log('Message deleted from SQS');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Processing completed successfully!' }),
+    };
+  } catch (error) {
+    console.error('Error processing message:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error processing event' }),
+    };
+  }
 };
